130b168a94d959347d6957179be64df933c2c0df
Rahul Gupta <rahulgupta201299@gmail.com>
2021-05-18 19:33:48 +0530

    update

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import path from 'path'
-import connect from 'connect'
-import serveStatic from 'serve-static'
+
 const app=express()
 var __dirname = path.resolve()
 app.use(express.static(path.join(__dirname,'build')))
 
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, 'build','index.html'));
+});
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build','index.html'));
-  });
-
-const port=8000||process.env.PORT
+const port=process.env.PORT||8000
 
 app.listen(port,()=>console.log(`listening to port ${port}`))
